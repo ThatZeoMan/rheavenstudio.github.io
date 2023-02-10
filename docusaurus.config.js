0daa3b049b9af140512db72ce50609e89f766c85
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-09 19:56:20 -0500

    Lets see

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,18 +10,20 @@ const config = {
   favicon: 'img/appLogo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/RHeavenStudio.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'RHeavenStudio', // Usually your GitHub org/user name.
+  projectName: 'RHeavenStudio.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
