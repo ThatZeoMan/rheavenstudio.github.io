7a316f4e013cb4493e805dc9173147cca5671e0e
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-10 14:31:40 -0500

    test out something

--- docs-contributing/intro.md
+++ docs-contributing/intro.md
@@ -9,3 +9,7 @@ Heaven Studio is written in the C# programming language using the Unity game eng
 If you are unfamiliar with C# or Unity, it is recommended that you take the time to create a small game using these first. That will provide you with more knowledge and understanding than any written explanation could.
 
 If you understand the basics of both, let’s get started.
+
+import {HeaderBadgesWidget} from '../src/components/HeaderWidget.js';
+
+<HeaderBadgesWidget />
---
+++ src/components/HeaderWidget.js
@@ -0,0 +1,45 @@
+import React from "react";
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+export const HeaderBadgesWidget = ({ commaDelimitedContributors, lastVerifiedDateString, lastVerifiedVersionString }) => {
+    let githubUsernames = {
+		Saraistupid: "Saraistupid",
+        Zeo: "ThatZeoMan",
+        Starpelly: "Starpelly"
+	}
+
+    let buildAuthorBadge = function (authorNickname) {
+		return (
+			<a class="header-badge" href={`https://github.com/${githubUsernames[authorNickname]}`}>
+				<span class="badge-avatar" style={{ backgroundImage: "url('https://avatars.githubusercontent.com/" + githubUsernames[authorNickname] + "')" }}></span>
+				<span class="badge-label">{authorNickname}</span>
+			</a>
+		)
+	}
+
+    let buildLastVerifiedBadge = function (dateString, versionString) {
+		if (dateString != null && versionString != null) {
+			return (
+				<a class="header-badge">
+					<span class="badge-avatar emoji-avatar">✔️</span>
+					<span class="badge-label">Last verified on <strong>{dateString}</strong> using HS {versionString}</span>
+				</a>
+			)
+		}
+	}
+
+    return (
+		<BrowserOnly>
+			{() =>
+				<div class="header-badges">
+					{(commaDelimitedContributors != null ? commaDelimitedContributors.split(',').map(buildAuthorBadge) : null)}
+					{buildLastVerifiedBadge(lastVerifiedDateString, lastVerifiedVersionString)}
+					<a class="header-badge" href={`https://github.com/rheavenstudio/rheavenstudio.github.io/issues/new?title=Docs update request: ${new URL(window.location.href).pathname}&body=Source: ${window.location.href}%0A%0ARequest: (how can we help?)`}>
+						<span class="badge-avatar emoji-avatar">✏️</span>
+						<span class="badge-label">Request an update</span>
+					</a>
+				</div>
+			}
+		</BrowserOnly>
+	);
+};
