aedd40089140a353b0975e760adbfacc0146ae96
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-10 12:30:31 -0500

    dead url lol

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,13 @@ export default function Home() {
           <meta property="og:type" content="website"/>
           <meta property="og:url" content="https://rheavenstudio.github.io/"/>
           <meta property="og:description" content="Heaven Studio is a (WIP) Rhythm Heaven level editor made in Unity."/>
-          <meta property="og:image" content="https://github.com/img/HSBanner.png"/>
+          <meta property="og:image" content="https://raw.githubusercontent.com/RHeavenStudio/rheavenstudio.github.io/main/static/img/HSBanner.png"/>
 
 
           <meta property="twitter:card" content="summary_large_image"/>
           <meta property="twitter:url" content="https://rheavenstudio.github.io/"/>
           <meta property="twitter:description" content="Heaven Studio is a (WIP) Rhythm Heaven level editor made in Unity."/>
-          <meta property="twitter:image" content="https://github.com/img/HSBanner.png"/>
+          <meta property="twitter:image" content="https://raw.githubusercontent.com/RHeavenStudio/rheavenstudio.github.io/main/static/img/HSBanner.png"/>
         </Head>
         <Redirect to='/docs/intro' />
     </Layout>
